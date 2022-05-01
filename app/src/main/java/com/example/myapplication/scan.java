0ba98449f6643383e1994b94bf2de068ff8f7957
ViswatejaA <103469591+ViswatejaA@users.noreply.github.com>
2022-04-30 20:25:15 -0500

    added qr scanner

--- app/src/main/java/com/example/myapplication/scan.java
+++ app/src/main/java/com/example/myapplication/scan.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class scan extends AppCompatActivity {
     Button qr;
     Button scan;
     Button contacts;
     Button settings;
+    Button btScan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,5 +53,30 @@ public class scan extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btScan = findViewById(R.id.qrScan);
+
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(scan.this);
+                intentIntegrator.setPrompt("Volume up to turn flash on\nVolume down to turn flash off");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data );
+
+        if(intentResult.getContents() != null){
+            //what to do with info
+            //intentREsult.getContents()
+        }
     }
 }
